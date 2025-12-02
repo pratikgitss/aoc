4725ebec6fb5d8b4486da7c317ddb821f46cf71c
Pratik Wasekar <pratikwasekar13@gmail.com>
2025-12-02 23:33:35 +0530

    day 1 sol 2

--- 2025/day1/go/sol2.go
+++ 2025/day1/go/sol2.go
@@ -1,7 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Println("hello")
+
+	f, err := os.ReadFile("input.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	lines := strings.Split(strings.TrimSpace(string(f)), "\n")
+	pass := 0
+	dial := 50
+	for _, line := range lines {
+		dir := line[0]
+		num, _ := strconv.Atoi(line[1:])
+
+		clicks := num / 100
+		pass += clicks
+
+		mod := num % 100
+		if mod == 0 {
+			continue
+		}
+
+		if dir == 'L' {
+			distToZero := dial
+			if dial == 0 {
+				distToZero = 100
+			}
+			if mod >= distToZero {
+				pass++
+			}
+			sub := dial - mod
+			if sub < 0 {
+				sub += 100
+			}
+			dial = sub
+		} else {
+			distToZero := 100 - dial
+
+			if dial == 0 {
+				distToZero = 100
+			}
+
+			if mod >= distToZero {
+				pass++
+			}
+
+			dial = (dial + mod) % 100
+		}
+	}
+
+	fmt.Println(pass)
 }
